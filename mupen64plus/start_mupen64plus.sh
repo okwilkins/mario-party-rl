e445a83449143062b3aa86836c05ce9932ce990f
Oliver Wilkins <okwilkins@googlemail.com>
2024-02-12 17:26:27 +0000

    Audio + networking + websocketfy

--- mupen64plus/start_mupen64plus.sh
+++ mupen64plus/start_mupen64plus.sh
@@ -2,7 +2,7 @@
 
 # Start pulseaudio server
 pulseaudio -D --verbose --exit-idle-time=-1 --disallow-exit
-pactl load-module module-native-protocol-tcp auth-anonymous=1
+pactl load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket
 
 # Start the emulator
 mupen64plus --nospeedlimit --resolution 640x480 roms/mario_party_usa.z64
--- vnc_server/start_vnc_server.sh
+++ vnc_server/start_vnc_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 pulseaudio --start --disallow-exit -vvv
-pasystray
 x11vnc -forever -viewonly -shared -nopw -noshm
